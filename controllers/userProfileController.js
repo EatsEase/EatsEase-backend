8aee8945d62a37345864159e02cc132383fe7def
Siravich Termvadsayanon <93331101+koonwill@users.noreply.github.com>
2025-01-12 14:24:58 +0700

    finish userProfile

---
+++ controllers/userProfileController.js
@@ -0,0 +1,55 @@
+const userProfileModel = require('../models/userProfileModel');
+
+const getUserProfileHandler = async (req, res) => {
+    try {
+        const userProfile = await userProfileModel.findOne({ username: req.params.username });
+        if (userProfile) {
+            res.status(200).json(userProfile);
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server Error" });
+    }
+}
+
+const updateUserProfileHandler = async (req, res) => {
+    try {
+        const userProfile = await userProfileModel.findOne({ username: req.params.username });
+        if (userProfile && userProfile.validate(req.body)) {
+            const updatedUserProfile = await userProfileModel.findOneAndUpdate({ username: req.params.username }, req.body, { new: true });
+            res.status(200).json(updatedUserProfile);
+        }
+        else {
+            res.status(404).json({ message: "User Profile not found" });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server Error" });
+    }
+}
+
+const updateReactionToMenuHandler = async (req, res) => {
+    try {
+        const userProfile = await userProfileModel.findOne({ username: req.params.username });
+        if (userProfile) {
+            //update only like and dislike
+            if (!req.body.liked_menu || !req.body.disliked_menu) {
+                res.status(400).json({ message: "Invalid request" });
+            }
+            else {
+                userProfile.liked_menu = req.body.liked_menu;
+                userProfile.disliked_menu = req.body.disliked_menu;
+                const updatedUserProfile = await userProfileModel.findOneAndUpdate({ username: req.params.username }, userProfile, { new: true });
+                res.status(200).json(updatedUserProfile);
+            }
+        }
+        else {
+            res.status(404).json({ message: "User Profile not found" });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server Error" });
+    }
+}
+
+module.exports = { getUserProfileHandler, updateUserProfileHandler, updateReactionToMenuHandler };
--- routes/userProfileRoutes.js
+++ routes/userProfileRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { getUserProfileHandler, updateUserProfileHandler } = require('../controllers/userProfileController');
+const { getUserProfileHandler, updateUserProfileHandler, updateReactionToMenuHandler } = require('../controllers/userProfileController');
 
 const router = express.Router();
 
 router.route('/:username').get(getUserProfileHandler);
 router.route('/edit/:username').put(updateUserProfileHandler);
-router.route('/like/:username').put(updateUserProfileHandler);
+router.route('/reactions/:username').put(updateReactionToMenuHandler);
+
+module.exports = router;
